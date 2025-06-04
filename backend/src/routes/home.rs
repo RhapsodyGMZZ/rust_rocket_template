0faae44df4341d4081d3ff2e73a4fddb3ce4bca4
RhapsodyGMZZ <nic.legay.76@protonmail.com>
2025-06-04 12:37:57 +0200

    Added small landing page, static folder to showcase the fileserver previously implemented, css folder too

--- backend/src/routes/home.rs
+++ backend/src/routes/home.rs
@@ -4,6 +4,8 @@ use rocket_dyn_templates::{context, Template};
 #[get("/")]
 pub fn home() -> Template {
     return Template::render("home", context! {
-        test:"Testing context"
+        description:"This is Rust's Rocket template that will be maintained and updated often. Feel free to make issues, 
+        give some ideas to enhance this little project.",
+        username: "RhapsodyGMZZ"
     })
 }
